90d805385019715b259bd15051ee48e8c2f8bb80
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-20 14:57:17 -0300

    make activityArray

--- input/main.js
+++ input/main.js
@@ -3,7 +3,7 @@ import { displayArray } from './display.js'
 import {addPlayerButton, editPlayerButton} from './window.js'
 import './darkmode.js'
 
-
+window.isHistoryNotApplied = true;
 window.isWindowNotOpen = true;
 
 document.querySelector('header > button').addEventListener('click',addPlayerButton);
@@ -19,10 +19,12 @@ export class Player {
 
 
 if (localStorage.playerScores) {
-  window.playerScores = JSON.parse(localStorage.playerScores)
+  window.playerScores = JSON.parse(localStorage.playerScores);
+  window.activityArray = [playerScores];
 }
 else {
-  window.playerScores = [new Player('gabbeeto', 1),new Player('memo', 2,1),new Player('jay', 5,2)]
+  window.playerScores = [new Player('gabbeeto', 1),new Player('memo', 2,1),new Player('jay', 5,2)];
+  window.activityArray = [playerScores];
 }
 
 
