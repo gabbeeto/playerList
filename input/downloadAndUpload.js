dce8dcbdeaec38466ace78b09069f379a500019d
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-20 17:36:30 -0300

    make it so you can upload the list

--- input/downloadAndUpload.js
+++ input/downloadAndUpload.js
@@ -1,13 +1,29 @@
+import {displayArray} from './display.js'
+
+
 var FileSaver = require('file-saver');
 const downloadButton = document.querySelector('header > button:nth-of-type(4)');
+const uploadButton = document.querySelector(`header > input[type='file']`);
 
-downloadButton.addEventListener('click',downloadFile)
+downloadButton.addEventListener('click', downloadFile)
+uploadButton.addEventListener('change', uploadFile)
 
-function downloadFile(){
+function downloadFile() {
 
-let blob = new Blob([JSON.stringify(playerScores)], {type: "text/json;charset=utf-8"});
-FileSaver.saveAs(blob, "playerList.json");
+  let blob = new Blob([JSON.stringify(playerScores)], { type: "text/json;charset=utf-8" });
+  FileSaver.saveAs(blob, "playerList.json");
 
 
 }
 
+
+function uploadFile() {
+  let fileReader = new FileReader();
+  let uploadedFile = document.querySelector(`input[type='file']`).files[0]
+
+  fileReader.readAsText(uploadedFile)
+  fileReader.onload = () => {
+    playerScores = JSON.parse(fileReader.result)
+    displayArray()
+  }
+}
