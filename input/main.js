72ed77800ae564f6074fe43f4a35506133bec7a3
Gabbeeto <124828006+gabbeeto@users.noreply.github.com>
2023-09-20 16:15:16 -0300

    make it so you can go back

--- input/main.js
+++ input/main.js
@@ -4,6 +4,7 @@ import { addPlayerButton, editPlayerButton } from './window.js'
 import './darkmode.js'
 
 let newArray;
+let indexo;
 window.isHistoryNotApplied = true;
 window.isWindowNotOpen = true;
 
@@ -24,12 +25,14 @@ if (localStorage.playerScores) {
   // I do this because of the reference problem when you try to directly put the array
   newArray = JSON.stringify(playerScores);
   window.activityArray = [JSON.parse(newArray)];
+  indexo = activityArray.length - 1;
 }
 else {
   window.playerScores = [new Player('gabbeeto', 1), new Player('memo', 2, 1), new Player('jay', 5, 2)];
   // I do this because of the reference problem when you try to directly put the array
   newArray = JSON.stringify(playerScores);
   window.activityArray = [JSON.parse(newArray)];
+  indexo = activityArray.length - 1;
 }
 
 
@@ -41,8 +44,64 @@ export function updateLocalStorage() {
 
 
 export function pushToActivityArray() {
-
+if(indexo == activityArray.length -1 ){
   // I do this because of the reference problem when you try to directly put the array
   newArray = JSON.stringify(playerScores);
   activityArray.push(JSON.parse(newArray))
+  indexo++
+  // console.log(indexo)
+  console.log(activityArray)
+  }
+  else{
+
+    // for(let index in activityArray){
+    //  if(index > indexo){
+    //     activityArray.splice(index,1)
+    //   }
+    // }
+    
+    console.log(`indexo:${indexo}`);
+    console.log(`activityArray.length:${activityArray.length}`);
+    console.log(`activityArray:`);
+    console.log(activityArray);
+
+  activityArray.length = indexo + 1;
+
+    console.log(`activityArray.length:${activityArray.length}`);
+
+    console.log(`activityArray:`);
+    console.log(activityArray);
+
+    console.log(`indexo:${indexo}`);
+
+    console.log(activityArray);
+  newArray = JSON.stringify(playerScores);
+  activityArray.push(JSON.parse(newArray))
+  indexo++
+  console.log(activityArray)
+
+  }
+}
+
+document.addEventListener('keydown', moveBackwardsAndForWardsWithActivityArray)
+
+function moveBackwardsAndForWardsWithActivityArray(event) {
+
+  switch (event.keyCode) {
+    case 90:
+      if (indexo != 0) {
+
+        console.log(event)
+        indexo = indexo -1
+        let stringo = JSON.stringify(activityArray[indexo])
+        playerScores = JSON.parse(stringo);
+        console.log(indexo)
+        displayArray()
+      }
+
+
+
+  }
+
+
 }
